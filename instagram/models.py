ef5021d44f8995dd2707afcb6699b5ee550cf8d7
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-05 03:09:33 +0300

    create models for class user

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    """User model."""
+
+
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=50)
+    
+
+    bio = models.TextField(blank=True)
+
+    birthdate = models.DateField(blank=True, null=True)
+    
+    is_admin = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
