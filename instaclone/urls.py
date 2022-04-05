7b3d36fc04a5625923e02ba5685ed948914a6ec3
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-05 23:21:25 +0300

    update forms.py

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,5 +1,3 @@
-# 
-# 
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views 
@@ -7,7 +5,8 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('clone.urls')),
+    path('',include('instagram.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
     path('tinymce/', include('tinymce.urls')),
+
 ]
--- instagram/forms.py
+++ instagram/forms.py
@@ -1,16 +1,32 @@
-"""Posts forms"""
-
-# Django
+from .models import  Post,Comment
 from django import forms
+from django.contrib.auth.models import User
 
-# Models
-from posts.models import Post
+class NewPostForm(forms.ModelForm):
+ 
+    class Meta:
+        model = Post
+        exclude = [ 'pub_date','liked', 'comments']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment',]
+        
+        
+class CreateUserForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+        'password': forms.PasswordInput(),
+    }
+        model = User
+        exclude = ['user_id','posts']
+
+        
+class UserUpdateForm(forms.ModelForm):
 
 
-class PostForm(forms.ModelForm):
-	"""Post model form"""
+    class Meta:
+        model = User
 
-	class Meta:
-		"""Form settings."""
-		model = Post
-		fields = ('profile', 'title', 'photo')
+        fields = ['password', 'email','username', 'first_name', 'last_name']
