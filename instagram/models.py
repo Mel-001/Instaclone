2ec76c5568cf3389268df1f156fa264e819794d0
Melonie Langat <meloni.langat@student.moringaschool.com>
2022-04-05 23:26:59 +0300

    update models.py

--- instagram/models.py
+++ instagram/models.py
@@ -1,48 +1,105 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 from cloudinary.models import CloudinaryField
 
-# Create your models here.
 
-    
+class Profile(models.Model):
+    user_id = models.BigAutoField(primary_key=True,)
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    username = models.CharField(max_length =30, blank=True)
+    password = models.CharField(max_length=50, default=0000, blank=False)
+    email = models.EmailField()
+    posts = models.ForeignKey('Post',on_delete=models.CASCADE,default=None)
+    profile_pic = CloudinaryField('image')
+    bio = models.TextField()
+
+    @classmethod
+    def get_users(cls):
+        users=cls.objects.all()
+        return users
+
+
+    def __str__(self):
+        return self.first_name
+
 class Post(models.Model):
-    """Post Model."""
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = CloudinaryField('image')
+    liked = models.ManyToManyField(User, default=None,blank= True,related_name='liked')
+    author =  models.ForeignKey(User,on_delete=models.CASCADE,related_name='author',blank= True,default=None)
 
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    profile = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    title = models.CharField(max_length=255)
-    photo = CloudinaryField('image')
+ 
+    def save_post(self):
+     self.save()
+             
+    def delete_post(self):
+     self.delete()
+    def __str__(self):
+        return self.user
     
+   
 
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
+
+    
+    @classmethod
+    def all_posts(cls):
+     '''
+     get all posts
+     
+     '''
+     posts=cls.objects.all()
+     return posts
+
+    @classmethod
+    def get_one_post(cls):
+     post=cls.objects.filter(id=id)
+     return post
+
+    @classmethod
+    def search_post(cls,search_term):
+     post = cls.objects.filter(name__icontains=search_term)
+     return post
+
+
+    @property
+    def like_count(self):
+        return self.liked.all().count()
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post',related_name='comments',on_delete=models.CASCADE,default=None)
+    comment = models.CharField(max_length = 200,  blank=True)
+
+
+    def save_comment(self):
+        self.save()
+
+    @classmethod
+    def get_comments(cls):
+        comments=cls.objects.all()
+        return comments
+
+class Like(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=None)
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,default=None)
+    like_value = models.CharField(choices=LIKE_CHOICES,  default='Like', blank=True, max_length=20)
+
+    def add_like(self):
+        self.save()
+
 
     def __str__(self):
-        """Return title and username"""
-        return "{} by @{}".format(self.title, self.profile.user.username)
-    
-class User(models.Model):
-    """User model."""
+        return self.likes
 
 
 
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=50)
+
     
 
-    bio = models.TextField(blank=True)
-
-    birthdate = models.DateField(blank=True, null=True)
-    
-    is_admin = models.BooleanField(default=False)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        """Return email."""
-        return self.email
